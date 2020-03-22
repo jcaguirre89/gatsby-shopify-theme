8f63760a1271901419e0d8f250800d9e0ad6ceba
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2020-03-22 19:27:23 -0300

    new index page

--- gatsby-theme-shopify/src/pages/index.js
+++ gatsby-theme-shopify/src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../components/layout';
 import StickyHero from '../components/StickyHero';
+import ProductList from '../components/ProductList';
 
 export default function Home() {
   return (
     <Layout>
       <StickyHero />
+      <ProductList />
     </Layout>
   );
 }
