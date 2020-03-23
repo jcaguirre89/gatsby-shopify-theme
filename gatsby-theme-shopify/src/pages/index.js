d6a4f64f057914abb2b1cd0ff0b0d480c5643fbe
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2020-03-23 13:41:19 -0300

    update

--- gatsby-theme-shopify/src/pages/index.js
+++ gatsby-theme-shopify/src/pages/index.js
@@ -6,7 +6,7 @@ import ProductList from '../components/ProductList';
 
 const Content = styled.div`
   margin: 0;
-  transform: translateY(50%);
+  /* transform: translateY(50%); */
   z-index: 2000;
   background: ${props => props.theme.white};
 `;
