cd06f9ed98fdad05296ac7540d004d7459e3e67b
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2020-03-28 02:08:22 -0300

    hero component

---
+++ gatsby-theme-shopify/src/components/Hero.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import Img from 'gatsby-image';
+
+const handleHeroContentLocation = location => {
+  switch (location) {
+    case 'left':
+      return 'justify-content: flex-start';
+    case 'right':
+      return 'justify-content: flex-end';
+    case 'center':
+      return 'justify-content: center';
+    default:
+      return 'justify-content: flex-start';
+  }
+};
+
+const HeroContainer = styled.section`
+  width: 100%;
+  height: 90vh;
+  position: relative;
+`;
+
+const Content = styled.div`
+  display: flex;
+  ${({ location }) => handleHeroContentLocation(location)};
+  align-items: center;
+  position: absolute;
+  padding: 100px;
+  width: 100%;
+  transform: translateY(-40%);
+  top: 50%;
+  color: ${props => props.color};
+
+  h2 {
+    font-size: 3rem;
+    font-family: 'Spartan';
+  }
+
+  h3 {
+    font-family: 'Spartan';
+    font-size: 2rem;
+  }
+
+  @media (max-width: ${props => props.theme.breakpoints.m}) {
+    transform: translateY(0);
+    justify-content: center;
+
+    h2 {
+      font-size: 2.5rem;
+    }
+
+    h3 {
+      font-size: 2rem;
+    }
+  }
+`;
+
+export default function Hero({
+  imageFluid,
+  title,
+  color,
+  contentLocation,
+  subtitle,
+  ctas,
+}) {
+  // Image must be fluid!
+  return (
+    <HeroContainer>
+      <Img fluid={imageFluid} style={{ height: '100%' }} />
+      <Content color={color} location={contentLocation}>
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        {ctas && ctas.map(cta => <p>cta placeholder</p>)}
+      </Content>
+    </HeroContainer>
+  );
+}
+
+Hero.propTypes = {
+  imageFluid: PropTypes.object.isRequired,
+  color: PropTypes.string,
+  title: PropTypes.string,
+  contentLocation: PropTypes.oneOf(['left', 'right', 'center']),
+  subtitle: PropTypes.string,
+  ctas: PropTypes.array,
+};
+
+Hero.defaultProps = {
+  title: '',
+  subtitle: '',
+  ctas: [],
+  color: 'white',
+  contentLocation: 'right',
+};
--- gatsby-theme-shopify/src/templates/CollectionPage.js
+++ gatsby-theme-shopify/src/templates/CollectionPage.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
+import Img from 'gatsby-image';
 import Layout from '../components/layout';
 import Header from '../components/header';
 import ProductList from '../components/ProductList';
+import Hero from '../components/Hero';
 
 export default function CollectionPage({ data }) {
-  const { title, products } = data.shopifyCollection;
+  const { title, image, products } = data.shopifyCollection;
+  console.log('a', title);
   return (
     <Layout>
-      <Header smart={false} />
+      <Header smart />
+      <Hero
+        contentLocation="right"
+        title={title}
+        imageFluid={image.localFile.childImageSharp.fluid}
+      />
       <ProductList products={products} />
     </Layout>
   );
@@ -18,6 +27,15 @@ export const query = graphql`
   query CollectionQuery($handle: String!) {
     shopifyCollection(handle: { eq: $handle }) {
       title
+      image {
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 1400) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
       products {
         availableForSale
         description
@@ -31,7 +49,7 @@ export const query = graphql`
           localFile {
             childImageSharp {
               fluid(maxWidth: 910) {
-                ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                ...GatsbyImageSharpFluid_withWebp
               }
             }
           }
