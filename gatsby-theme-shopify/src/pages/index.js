14cd674dc0725862acbf6bcab64c2a7d99be72d1
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2020-03-28 01:13:43 -0300

    product list as data independat component

--- gatsby-theme-shopify/src/pages/index.js
+++ gatsby-theme-shopify/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import StickyHero from '../components/StickyHero';
 import ProductList from '../components/ProductList';
@@ -10,14 +11,42 @@ const Content = styled.div`
   background: ${props => props.theme.white};
 `;
 
-export default function Home() {
+export default function Home({ data }) {
+  const products = data.products.nodes;
+  console.log(products);
   return (
     <Layout>
       <Header smart />
       <StickyHero />
       <Content>
-        <ProductList />
+        <ProductList products={products} />
       </Content>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query allProducts {
+    products: allShopifyProduct(sort: { order: DESC, fields: handle }) {
+      nodes {
+        title
+        description
+        handle
+        availableForSale
+        variants {
+          id
+          price
+        }
+        images {
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 910) {
+                ...GatsbyImageSharpFluid_withWebp_tracedSVG
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
