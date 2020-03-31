fefc834f81d0f923a458275f6445d3c8a9130794
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2020-03-31 16:37:48 -0300

    rename gatsby env vars

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -9,9 +9,9 @@ module.exports = {
       options: {
         shopName: process.env.GATSBY_SHOP_NAME,
         accessToken: process.env.GATSBY_SHOPIFY_ACCESS_TOKEN,
-        sanityProjectId: process.env.SANITY_PROJECT_ID,
-        sanityDataset: process.env.SANITY_DATASET,
-        sanityToken: process.env.SANITY_TOKEN
+        sanityProjectId: process.env.GATSBY_SANITY_PROJECT_ID,
+        sanityDataset: process.env.GATSBY_SANITY_DATASET,
+        sanityToken: process.env.GATSBY_SANITY_TOKEN
       }
     }
   ]
