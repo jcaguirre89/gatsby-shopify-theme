e94be413b1463bfdfb0180cf8f8af5e7d10948bd
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2020-04-01 00:14:39 -0300

    Made about page

--- gatsby-shopify-theme/src/pages/about.js
+++ gatsby-shopify-theme/src/pages/about.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
+import { graphql } from 'gatsby';
+import LandingPage from '../components/LandingPage';
 import Layout from '../components/layout';
+import Header from '../components/header';
 
-export default function About() {
+export default function Home({ data }) {
+  const landingPage = data && data.landingPage;
   return (
     <Layout>
-      <h2>About</h2>
-      <p>Placeholder</p>
+      <Header smart />
+      <LandingPage {...landingPage} />
     </Layout>
   );
 }
+
+export const query = graphql`
+  query AboutLandingQuery {
+    landingPage: sanityLandingPage(handle: { current: { eq: "about" } }) {
+      id
+      _rawBody
+      title
+      subtitle
+      cta {
+        link
+        text
+      }
+      handle {
+        current
+      }
+      mainImage {
+        crop {
+          _key
+          _type
+          top
+          bottom
+          left
+          right
+        }
+        hotspot {
+          _key
+          _type
+          x
+          y
+          height
+          width
+        }
+        asset {
+          _id
+        }
+        alt
+      }
+    }
+  }
+`;
